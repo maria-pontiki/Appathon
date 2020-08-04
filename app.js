8f69e08c335aa504c8e0c3a60ab0657663e3e430
Maria Pontiki <mariapontikiiii@hotmail.com>
2020-08-04 21:51:30 +0300

    Read clinical trials for MongoDB

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 var express = require("express");
 var path = require("path");
-
+//var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
 var routes = require("./routes");
 var app = express();
+
+//const url = 'mongodb://localhost:27017/clinicalTrialsTwo'
+//mongoose.connect(url, {useUnifiedTopology:true, useNewUrlParser:true, useCreateIndex:true});
 
 app.set("port", process.env.PORT || 3000);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(session({
+    secret:"doemlfgddfsoi!gjdsf5684561dsf",
+    resave:false,
+    saveUninitialized:false
+}));
 
 app.use(routes);
 
--- routes.js
+++ routes.js
@@ -1,11 +1,53 @@
 var express = require("express");
-
+const MongoClient = require('mongodb').MongoClient
 var router = express.Router();
+
+const url = 'mongodb://localhost:27017/clinicalTrialsTwo';
+
+
+MongoClient.connect(url, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('clinicalTrialsTwo')
+    const trialsCollection = db.collection('trialDataTwo')
+
+    router.post("/clinical_trial", function (req, res, next) {
+        var condition = req.body.condition;
+        var country = req.body.country;
+	var query_dict = {"clinical_study.condition" : condition,
+                                  "clinical_study.location_countries.country" : country,
+		                  "clinical_study.enrollment.@type" : "Actual",
+                                  $or:[
+                                         {"clinical_study.overall_status" : "Completed"},
+                                         {"clinical_study.overall_status" : "Terminated"}
+                                      ]}
+
+        trialsCollection.find(query_dict).toArray(function(err, documents) {
+		var i;
+		var sum = 0;
+                for (i = 0; i < documents.length; i++) {
+                    if ("enrollment" in documents[i].clinical_study){
+		        sum = sum + parseInt(documents[i].clinical_study.enrollment["#text"]);
+		    }
+                }
+		console.log(sum);
+        });
+    });
+	  
+  })
+  .catch(console.error)
+
+
 
 router.get("/",function(req,res){
 
 	res.render("index");
 });
+
+router.get("/clinical_trial", function (req, res) {
+   res.render("clinical_trial")
+});
+
 
 
 module.exports = router;
