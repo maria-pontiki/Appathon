d135ebe14cdc8777c70ac900fc3d4c6b3dafb051
Maria Pontiki <mariapontikiiii@hotmail.com>
2020-08-12 22:28:42 +0300

    Add Table with results

--- routes.js
+++ routes.js
@@ -13,7 +13,9 @@ MongoClient.connect(url, { useUnifiedTopology: true })
 
     router.post("/clinical_trial", function (req, res, next) {
         var condition = req.body.condition;
-        var country = req.body.country;
+        var country = req.body.country; 
+	if ( condition == "" || country == "") { return res.json({status: "500"}); }
+	var obj_array = [];
 	var query_dict = {"clinical_study.condition" : condition,
                                   "clinical_study.location_countries.country" : country,
 		                  "clinical_study.enrollment.@type" : "Actual",
@@ -25,13 +27,31 @@ MongoClient.connect(url, { useUnifiedTopology: true })
         trialsCollection.find(query_dict).toArray(function(err, documents) {
 		var i;
 		var sum = 0;
+
+		if (documents.length == 0){return res.json({status: "400"});}
+
                 for (i = 0; i < documents.length; i++) {
                     if ("enrollment" in documents[i].clinical_study){
 		        sum = sum + parseInt(documents[i].clinical_study.enrollment["#text"]);
+			//console.log(documents[i].clinical_study.id_info.nct_id);
+			//console.log(documents[i].clinical_study.overall_status);
+
+			obj_array.push({"condition": condition,
+				        "country": country,
+				        "id": documents[i].clinical_study.id_info.nct_id,
+				        "status": documents[i].clinical_study.overall_status,
+				        "enrollments": documents[i].clinical_study.enrollment["#text"]
+			               });
+
+			//console.log(obj_array)
+			
 		    }
                 }
 		//console.log(sum.toString());
-		return res.json({result: sum.toString()});
+		return res.json({result: sum.toString(),
+		                 table: obj_array,
+			         status: "200"
+		                });
         });
     });
 	  
